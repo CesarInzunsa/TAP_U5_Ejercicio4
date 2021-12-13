d94d34565813fe12a803b5702a87370fa41a72c2
Cesar Inzunsa <cesarinzunsa@hotmail.com>
2021-12-13 10:41:42 -0700

    fin

--- app/src/main/java/mx/edu/ittepic/tap_u5_ejercicio4/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/tap_u5_ejercicio4/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        cargarDatosDesdeNube()
+
         button.setOnClickListener {
             val documento = hashMapOf(
                 "NOMBRE" to nombre.text.toString(),
